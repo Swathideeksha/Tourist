14544db450f5b8b77df98e48b280a068bff799da
sushhbangera <sushmithard8@gmail.com>
2026-03-26 21:21:04 +0530

    Fix ESLint error: Remove unused Popup import

--- src/components/Rmap.jsx
+++ src/components/Rmap.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import L from "leaflet";
 import { useNavigate } from "react-router-dom";
 
