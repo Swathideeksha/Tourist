da6f24620c1d8110b3ce4e1ffa88f9036b3e8fbf
sushhbangera <sushmithard8@gmail.com>
2026-03-27 18:44:47 +0530

    Update hero images - Hero.jsx uses ww.png, PlaceHero.jsx uses www.jpg

--- src/components/PlaceHero.jsx
+++ src/components/PlaceHero.jsx
@@ -5,7 +5,7 @@ const PlaceHero = () => {
       <div 
         className="relative w-full h-[400px] md:h-[500px] rounded-3xl md:rounded-[3rem] shadow-2xl overflow-hidden"
         style={{
-          backgroundImage: 'url(/assets/im.jpg)',
+          backgroundImage: 'url(/assets/www.jpg)',
           backgroundSize: 'cover',
           backgroundPosition: 'center'
         }}
